94b472597d11d8ee03c1dac8979dca5e79720e5c
Rendy Artha <rendyartha312@gmail.com>
2021-03-22 14:02:44 +0700

    connecting database with express

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express()
 const port = process.env.port || 3000
 
 app.use(express.json())
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "onlineshop"
+})
+
+connection.connect((err) => {
+  if(err) {
+    console.log(err.sqlMessage)
+  } else {
+    console.log('Connection database success')
+  }
+})
+
 app.get('/', (req, res) => {
   res.json({
-    message: "Router run"
+    message: "Success"
   })
 })
 
