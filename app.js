ad21bfe25220ebaa2d73aac2d0e065836e3829a7
Rendy Artha <rendyartha312@gmail.com>
2021-03-22 13:57:58 +0700

    setup database

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express()
 const port = process.env.port || 3000
 
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "Router run"
+  })
+})
 
 app.listen(port, (req, res) => {
   console.log(`Application is running at http://localhost:${port}`)
